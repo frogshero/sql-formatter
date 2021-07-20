5698cf253872be77898f05809c99b6cba1b71abe
wangguofu@ym.com <wangguofu@ym.com>
2021-07-20 14:10:25 +0800

    java

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 formatter: sql formatter
 caller: java caller
+
+
+
+import aa.bb.cc.Test123;
+public class HelloWorld {                     
+private String aa;                         
+private Test123 bb;                        
+private String mm() {                      
+return aa;                             
+}                                          
+public static void main(String[] args) {   
+System.out.println(Test456.xx());   
+HelloWorld hw = new HelloWorld();       
+hw.aa = "ffffff";                     
+System.out.println(bb.mm());           
+}                                          
+}                
--- src/test/java/com/hubery/formatter/CallerTest.java
+++ src/test/java/com/hubery/formatter/CallerTest.java
@@ -1,18 +1,22 @@
 package com.hubery.formatter;
 
 public class CallerTest {
-    private String javaClz = "public class HelloWorld { " +
-            "   private String aa;" +
-            "   private String mm() {" +
-            "       return aa;" +
-            "   }" +
-            "   " +
-            "   public static void main(String[] args) { " +
-            "      System.out.println(\"Hello, World\");" +
-            "      " +
-            "      HelloWorld hw = new HelloWorld();" +
-            "      hw.aa = \"ffffff\";" +
-            "       System.out.println(hw.mm());" +
-            "   }" +
-            "}";
+    private String javaClz =
+            "public class HelloWorld {                      " +
+            "   private String aa;                          " +
+            "   private Test123 bb;                         " +
+            "   private String mm() {                       " +
+            "       return aa;                              " +
+            "   }                                           " +
+
+            "   public static void main(String[] args) {    " +
+            "      System.out.println(\"Hello, World\");    " +
+
+            "      HelloWorld hw = new HelloWorld();        " +
+            "      hw.aa = \"ffffff\";                      " +
+            "       System.out.println(bb.mm());            " +
+            "   }                                           " +
+            "}                                              ";
+
+
 }
